8c99d6bce41104546d24ea5afcb38a783954cf8a
Deathwolf772X <147344482+Deathwolf772X@users.noreply.github.com>
2024-07-25 14:51:09 +0100

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",  // <=== enables static exports
+    output: "export",
     reactStrictMode: true,
-  };
-  
-  module.exports = nextConfig;
+    images: {
+        unoptimized: true,
+    },
+};
+
+module.exports = nextConfig;
